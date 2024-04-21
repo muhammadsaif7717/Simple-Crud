8470b5f93b06b3a6dccdab686887a3d5b38861b1
MuHammad Saif <muhammadsaif7717@gmail.com>
2024-04-22 01:59:36 +0600

    Added Files

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5005;
@@ -11,12 +13,9 @@ app.use(express.json())
 
 
 
-//username
-// muhammadsaif7717
-//password
-// cFOyYQSkuZkBDDFg
 
-const uri = "mongodb+srv://muhammadsaif7717:cFOyYQSkuZkBDDFg@cluster0.oh0s98i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oh0s98i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
